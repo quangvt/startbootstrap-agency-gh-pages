a3d2af65bf16bd5e9a644f1dd918f38fee5e38cc
Vu Trong Quang <vutrongquang@gmail.com>
2018-12-24 20:25:33 +0700

    Add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./'));
+
+app.get('/test', (req, res) => {
+  return res.send('Hello, I am a webserver');
+});
+
+app.listen(3000);
