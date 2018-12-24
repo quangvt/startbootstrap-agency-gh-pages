53b4aabdfe289806e1cc72ff3d32feec6814ee58
Vu Trong Quang <vutrongquang@gmail.com>
2018-12-24 21:33:49 +0700

    Add pulic folder

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
-app.use(express.static('./'));
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/test', (req, res) => {
   return res.send('Hello, I am a webserver');
 });
 
-app.listen(80);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
